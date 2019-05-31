f73ba910f06588500b5b325286b82ab52061c777
Jeff Hutchison <jeff@jeffhutchison.com>
2019-05-31 07:44:10 -0400

    WIP - before create react app conversion

--- deadeye/static/js/App.jsx
+++ deadeye/static/js/App.jsx
@@ -1,22 +1,23 @@
 // App.jsx
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 export default class App extends React.Component {
-  render () {
+  render() {
     return (
-<Container>
-  <Row>
-    <Col>1 of 2</Col>
-    <Col>2 of 2</Col>
-  </Row>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col>2 of 3</Col>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+          <Col>4 of 4</Col>
+        </Row>
+      </Container>
     );
   }
 }
