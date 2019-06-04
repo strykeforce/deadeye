801f997d7f5847a780014eccf6ff87282ad4d2df
Jeff Hutchison <jeff@jeffhutchison.com>
2019-06-04 16:47:58 -0400

    Add stubbed-out view controls

--- deadeye/static/src/CameraStream.js
+++ deadeye/static/src/CameraStream.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 
 import standBy from "./deadeye.png";
 
@@ -10,12 +15,19 @@ const width = 322;
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(2)
+  },
+  formControl: {
+    margin: theme.spacing(3)
+  },
+  group: {
+    margin: theme.spacing(1, 0)
   }
 }));
 
 export default function CameraStream({ camera, displayName }) {
   const classes = useStyles();
   const [source, setSource] = useState(standBy);
+  const [value, setValue] = React.useState("female");
 
   useEffect(() => {
     if (camera.on) {
@@ -25,12 +37,51 @@ export default function CameraStream({ camera, displayName }) {
     }
   }, [camera.on, camera.streamUrl]);
 
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
   return (
     <Paper className={classes.root}>
-      <img border={1} src={source} width={width} alt="Stream" />
       {displayName && (
         <Typography variant="body1">Camera {camera.id}</Typography>
       )}
+      <img border={1} src={source} width={width} alt="Stream" />
+      <div className={classes.root}>
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">View</FormLabel>
+          <RadioGroup
+            name="view"
+            className={classes.group}
+            value="original"
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="original"
+              control={<Radio />}
+              label="Original"
+            />
+            <FormControlLabel value="mask" control={<Radio />} label="Mask" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">Contours</FormLabel>
+          <RadioGroup
+            name="view"
+            className={classes.group}
+            value="none"
+            onChange={handleChange}
+          >
+            <FormControlLabel value="none" control={<Radio />} label="None" />
+            <FormControlLabel
+              value="filter"
+              control={<Radio />}
+              label="Filtered"
+            />
+            <FormControlLabel value="all" control={<Radio />} label="All" />
+          </RadioGroup>
+        </FormControl>
+      </div>
     </Paper>
   );
 }
