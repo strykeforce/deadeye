2e09070c3a418d8ad7c3e0ea30e68bd62f7a41a5
Jeff Hutchison <jeff@jeffhutchison.com>
2019-08-24 09:51:03 -0400

    Add eslint-plugin-react-hooks

--- admin-web/.eslintrc.js
+++ admin-web/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  plugins: ['react-hooks'],
   parserOptions: {
     project: './tsconfig.json',
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -31,6 +32,8 @@ module.exports = {
     ],
     'react/prop-types': 'off',
     'linebreak-style': ['error', 'unix'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
     react: {
