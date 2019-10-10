74c90c49aa2b70bf500d77987589dded06f38481
Jeff Hutchison <jeff@jeffhutchison.com>
2019-10-10 15:49:57 -0400

    Enable multiple cameras in watch
    Setting link is not reliable, sometimes returns 127.0.0.1

--- cli/src/main/kotlin/org/strykeforce/deadeye/cli/Watch.kt
+++ cli/src/main/kotlin/org/strykeforce/deadeye/cli/Watch.kt
@@ -2,18 +2,20 @@ package org.strykeforce.deadeye.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.multiple
 import org.fusesource.jansi.Ansi.ansi
 import org.fusesource.jansi.AnsiConsole
 import org.jline.terminal.TerminalBuilder
 import org.strykeforce.deadeye.Deadeye
 import org.strykeforce.deadeye.TargetData
 import org.strykeforce.deadeye.TargetDataListener
+import java.net.Inet4Address
 
 private const val LINES = 4
 
 class Watch : CliktCommand(), TargetDataListener {
 
-  private val id by argument()
+  private val id by argument().multiple()
 
   override fun onTargetData(data: TargetData) {
     println(ansi().cursorUpLine(LINES).cursorToColumn(0).a("Serial: ${data.sn}"))
@@ -24,15 +26,21 @@ class Watch : CliktCommand(), TargetDataListener {
 
   override fun run() {
     AnsiConsole.systemInstall()
+    val prevLink = Deadeye.link
+    Deadeye.link = Deadeye.Link(Inet4Address.getLocalHost().hostAddress, 5800)
+    println("Redirecting target data stream to ${Deadeye.link}")
     repeat(LINES) { println() }
-    val camera = Deadeye.getCamera(id).also {
-      it.targetDataListener = this
-      it.enabled = true
+    val cameras = id.map {
+      Deadeye.getCamera(it).also { camera ->
+        camera.enabled = true
+      }
     }
+    cameras.first().targetDataListener = this
     val terminal = TerminalBuilder.builder().jansi(true).system(true).build()
     terminal.enterRawMode()
     terminal.reader().read()
-    camera.enabled = false
+    cameras.forEach { it.enabled = false }
+    Deadeye.link = prevLink
     terminal.close()
     AnsiConsole.systemUninstall()
   }
