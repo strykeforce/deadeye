4d1cfe1d248bd95bf9fa90e223c4ca095f84b6f6
Jeff Hutchison <jeff@jeffhutchison.com>
2019-10-11 17:47:28 -0400

    Add FPS meter to watch subcommand

--- cli/src/main/kotlin/org/strykeforce/deadeye/cli/Watch.kt
+++ cli/src/main/kotlin/org/strykeforce/deadeye/cli/Watch.kt
@@ -1,33 +1,47 @@
 package org.strykeforce.deadeye.cli
 
+import com.codahale.metrics.Meter
+import com.codahale.metrics.MetricRegistry
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import org.fusesource.jansi.Ansi.ansi
 import org.fusesource.jansi.AnsiConsole
 import org.jline.terminal.TerminalBuilder
+import org.strykeforce.deadeye.Camera
 import org.strykeforce.deadeye.Deadeye
 import org.strykeforce.deadeye.TargetData
 import org.strykeforce.deadeye.TargetDataListener
-import java.net.Inet4Address
+import java.net.Socket
+import java.net.URL
+import java.text.MessageFormat
 
-private const val LINES = 4
+private const val LINES = 5
 
 class Watch : CliktCommand(), TargetDataListener {
 
   private val id by argument().multiple()
+  private val metrics = MetricRegistry()
+  private val fps: Meter by lazy { metrics.meter("messages") }
 
   override fun onTargetData(data: TargetData) {
+    fps.mark()
     println(ansi().cursorUpLine(LINES).cursorToColumn(0).a("Serial: ${data.sn}"))
     println(ansi().cursorToColumn(0).a(" Valid: ${data.valid}"))
-    println(ansi().cursorToColumn(0).a("     X: ${data.x}"))
-    println(ansi().cursorToColumn(0).a("     Y: ${data.y}"))
+    println(ansi().cursorToColumn(0).a("     X: ${data.x.toInt()}"))
+    println(ansi().cursorToColumn(0).a("     Y: ${data.y.toInt()}"))
+    println(ansi().cursorToColumn(0).a("   FPS: ${fps.oneMinuteRate.toInt()}"))
   }
+
+  private fun linkFromStream(stream: Camera.Stream): Deadeye.Link = Socket(URL(stream.url).host, 22).use {
+    Deadeye.Link(it.localAddress.hostAddress, 5800, true)
+  }
+
 
   override fun run() {
     AnsiConsole.systemInstall()
     val prevLink = Deadeye.link
-    Deadeye.link = Deadeye.Link(Inet4Address.getLocalHost().hostAddress, 5800)
+    Deadeye.link = linkFromStream(Deadeye.getCamera(id.first()).stream)
     println("Redirecting target data stream to ${Deadeye.link}")
     repeat(LINES) { println() }
     val cameras = id.map {
