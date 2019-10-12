757f66cd04420e7f7dac013d4ceea648f26784a8
Jeff Hutchison <jeff@jeffhutchison.com>
2019-10-12 17:32:25 -0400

    Add ability to watch multiple cameras

--- cli/src/main/kotlin/org/strykeforce/deadeye/cli/Watch.kt
+++ cli/src/main/kotlin/org/strykeforce/deadeye/cli/Watch.kt
@@ -14,49 +14,71 @@ import org.strykeforce.deadeye.TargetData
 import org.strykeforce.deadeye.TargetDataListener
 import java.net.Socket
 import java.net.URL
-import java.text.MessageFormat
+import kotlin.concurrent.timer
 
 private const val LINES = 5
 
-class Watch : CliktCommand(), TargetDataListener {
+private val metrics = MetricRegistry()
 
-  private val id by argument().multiple()
-  private val metrics = MetricRegistry()
-  private val fps: Meter by lazy { metrics.meter("messages") }
+class Watch : CliktCommand() {
 
-  override fun onTargetData(data: TargetData) {
-    fps.mark()
-    println(ansi().cursorUpLine(LINES).cursorToColumn(0).a("Serial: ${data.sn}"))
-    println(ansi().cursorToColumn(0).a(" Valid: ${data.valid}"))
-    println(ansi().cursorToColumn(0).a("     X: ${data.x.toInt()}"))
-    println(ansi().cursorToColumn(0).a("     Y: ${data.y.toInt()}"))
-    println(ansi().cursorToColumn(0).a("   FPS: ${fps.oneMinuteRate.toInt()}"))
-  }
-
-  private fun linkFromStream(stream: Camera.Stream): Deadeye.Link = Socket(URL(stream.url).host, 22).use {
-    Deadeye.Link(it.localAddress.hostAddress, 5800, true)
-  }
-
+  private val ids by argument().multiple()
 
   override fun run() {
     AnsiConsole.systemInstall()
     val prevLink = Deadeye.link
-    Deadeye.link = linkFromStream(Deadeye.getCamera(id.first()).stream)
-    println("Redirecting target data stream to ${Deadeye.link}")
-    repeat(LINES) { println() }
-    val cameras = id.map {
-      Deadeye.getCamera(it).also { camera ->
-        camera.enabled = true
-      }
+    Deadeye.link = Deadeye.getCamera(ids.first()).stream.toLink()
+    println("ID  serial  valid     X     Y   FPS")
+    println("===================================")
+
+    val watchers = ids.map(Deadeye::getCamera).map(::Watcher)
+    watchers.forEach { _ -> println() }
+
+    val timer = timer(period = 250) {
+      print(ansi().cursorUpLine(watchers.size))
+      watchers.forEach(Watcher::output)
     }
-    cameras.first().targetDataListener = this
+
     val terminal = TerminalBuilder.builder().jansi(true).system(true).build()
     terminal.enterRawMode()
     terminal.reader().read()
-    cameras.forEach { it.enabled = false }
+
+    watchers.forEach { it.camera.enabled = false }
+    timer.cancel()
+
     Deadeye.link = prevLink
     terminal.close()
     AnsiConsole.systemUninstall()
   }
 }
 
+class Watcher(val camera: Camera) : TargetDataListener {
+
+  init {
+    camera.targetDataListener = this
+    camera.enabled = true
+  }
+
+  private var td = TargetData(camera.id)
+  private var msg = ""
+  private val fpsMeter: Meter by lazy { metrics.meter(camera.id) }
+
+  override fun onTargetData(data: TargetData) {
+    msg = if (data.sn - td.sn > 1) "dropped ${data.sn - td.sn}" else msg
+    td = data
+    fpsMeter.mark()
+  }
+
+  fun output() {
+    val sn = td.sn.toString().padStart(6)
+    val valid = td.valid.toString().padStart(6)
+    val x = td.x.toInt().toString().padStart(5)
+    val y = td.y.toInt().toString().padStart(5)
+    val fps = "%5.1f".format(fpsMeter.oneMinuteRate)
+    println(ansi().a("${td.id}  $sn $valid $x $y $fps  $msg"))
+  }
+}
+
+fun Camera.Stream.toLink(): Deadeye.Link = Socket(URL(this.url).host, 22).use {
+  Deadeye.Link(it.localAddress.hostAddress, 5800, true)
+}
