aac471ba4e978299a3403080783614f6e838a30b
Jeff Hutchison <jeff@jeffhutchison.com>
2019-11-10 08:34:28 -0500

    Configure logging for systemd if present

--- daemon/src/main.cpp
+++ daemon/src/main.cpp
@@ -1,5 +1,8 @@
-#include <spdlog/spdlog.h>
 #include <memory>
+#include <spdlog/spdlog.h>
+#ifdef __linux__
+#include <spdlog/sinks/systemd_sink.h>
+#endif
 
 #include "deadeye.h"
 
@@ -7,18 +10,27 @@ using namespace deadeye;
 
 namespace {
 void ConfigureLogging() {
+#ifdef __linux__
+  if (std::getenv("JOURNAL_STREAM")) { // started by systemd
+    spdlog::set_default_logger(spdlog::systemd_logger_st("deadeyed"));
+    spdlog::set_level(spdlog::level::info);
+    spdlog::set_pattern("[%t] %v");
+    spdlog::debug("JOURNAL_STREAM={}", std::getenv("JOURNAL_STREAM"));
+  }
+#endif
+
   spdlog::set_level(spdlog::level::debug);
   spdlog::set_pattern("[%H:%M:%S.%e] [%t] [%^%l%$]: %v");
 }
-}  // namespace
+} // namespace
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ConfigureLogging();
 
   try {
     DEADEYE_INIT()
     Controller::GetInstance().Run();
-  } catch (std::exception const& e) {
+  } catch (std::exception const &e) {
     spdlog::critical("Controller::Run exception: {}", e.what());
     std::exit(EXIT_FAILURE);
   }
