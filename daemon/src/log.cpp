e5e9b4839316d88c6820800333771dba912eaa6e
Jeff Hutchison <jeff@jeffhutchison.com>
2019-11-13 15:59:31 -0500

    Add better checks for journal stream

--- daemon/src/log.cpp
+++ daemon/src/log.cpp
@@ -1,19 +1,33 @@
 #include "log.h"
 
 #ifdef __linux__
+#include <fmt/core.h>
 #include <spdlog/sinks/systemd_sink.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstring>
 #endif
 
 void deadeye::log::Configure(std::string name) {
-  spdlog::set_level(spdlog::level::debug);
-  spdlog::set_pattern("[%H:%M:%S.%e] [%t] [%^%l%$]: %v");
-
 #ifdef __linux__
-  if (std::getenv("JOURNAL_STREAM")) {  // started by systemd
-    spdlog::set_default_logger(spdlog::systemd_logger_st(name));
-    spdlog::set_level(spdlog::level::info);
-    spdlog::set_pattern("[%t] %v");
-    spdlog::info("JOURNAL_STREAM={}", std::getenv("JOURNAL_STREAM"));
+  const char* journal = std::getenv("JOURNAL_STREAM");
+  if (journal) {  // started by systemd
+    struct stat es;
+    if (fstat(STDERR_FILENO, &es)) {
+      spdlog::error("Failed to stat stderr: {}", std::strerror(errno));
+      return;
+    }
+
+    if (fmt::format("{}:{}", es.st_dev, es.st_ino) == std::string{journal}) {
+      spdlog::set_default_logger(spdlog::systemd_logger_st(name));
+      spdlog::set_level(spdlog::level::info);
+      spdlog::set_pattern("[%t] %v");
+      spdlog::info("Configuring logging for systemd");
+      return;
+    }
   }
 #endif
+  spdlog::info("Configuring logging for console.");
+  spdlog::set_level(spdlog::level::debug);
+  spdlog::set_pattern("[%H:%M:%S.%e] [%t] [%^%l%$]: %v");
 }
