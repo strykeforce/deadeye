8f458b1b6a1b4e5dd7addeaab9c255a815e2fdc4
Jeff Hutchison <jeff@jeffhutchison.com>
2019-11-13 13:06:59 -0500

    Add shutdown via gpio button

--- daemon/src/log.h
+++ daemon/src/log.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <spdlog/spdlog.h>
+#include <string>
+
 namespace deadeye {
 namespace log {
-void Configure();
+void Configure(std::string name);
 }
 }  // namespace deadeye
