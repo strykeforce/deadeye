192d6dad338e1749072f87192316b0fbf8575a5e
Jeff Hutchison <jeff@jeffhutchison.com>
2020-01-14 18:55:25 -0500

    Update for 2020 wpilib

--- cli/settings.gradle.kts
+++ cli/settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/5.6.2/userguide/multi_project_builds.html
  */
 
-rootProject.name = "deadeye"
-includeBuild("../../thirdcoast")
+rootProject.name = "deadeye-cli"
+includeBuild("../deadeye-java")
