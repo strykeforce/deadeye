2fb0156f144bb39a4f0e6fa5b87f9bbbb3765a46
Jeff Hutchison <jeff@jeffhutchison.com>
2020-01-18 12:03:55 -0500

    Ignore Idea IDE files

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
   // Apply the Kotlin JVM plugin to add support for Kotlin.
   id("org.jetbrains.kotlin.jvm") version "1.3.61"
-
   // Apply the application plugin to add support for building a CLI application.
   application
-
   idea
 }
 
