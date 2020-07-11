22a89950917e78139483fe72d50b19d48a251c46
Jeff Hutchison <jeff@jeffhutchison.com>
2020-07-11 12:48:17 -0400

    Move src to /opt/deadeye

--- admin-server/setup.py
+++ admin-server/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deadeye",
-    version="20.2",
+    version="20.2.0", # keep in sync with ansible/roles/deadeye/vars/main.yaml 
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
