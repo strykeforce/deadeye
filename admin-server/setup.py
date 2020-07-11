1d531af87a7c7ef20991267b3d9ef6a2a0670eb3
Jeff Hutchison <jeff@jeffhutchison.com>
2020-07-11 17:54:27 -0400

    Add version bump reminder comment

--- admin-server/setup.py
+++ admin-server/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deadeye",
-    version="20.2.0", # keep in sync with ansible/roles/deadeye/vars/main.yaml 
+    version="20.2.0",  # keep in sync with ansible/roles/deadeye/vars/main.yaml, daemon/version.cmake
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
--- daemon/version.cmake
+++ daemon/version.cmake
@@ -1,1 +1,1 @@
-set(Deadeye_VER 20.2.0)
+set(Deadeye_VER 20.2.0) # keep in sync with admin-server/setup.py, ansible/roles/deadeye/vars/main.yaml
