13879be7617b0cf0f43b1163fe1e3ec78844dda6
Jeff Hutchison <jeff@jeffhutchison.com>
2020-07-23 11:37:38 -0400

    Add flags to bump version

--- scripts/bump.py
+++ scripts/bump.py
@@ -1,66 +1,105 @@
 #!/usr/bin/env python3
-"""Bump Deadeye to specified version."""
+"""Manage Deadeye version."""
 
 import argparse
 from pathlib import Path
+import re
 
 
-def do_cmake(version):
-    """ Bump cmake version file."""
-    cmake = Path(__file__).parent / ".." / "daemon" / "version.cmake"
-
-    with open(cmake, "r") as file:
-        lines = file.readlines()
-
-    lines[0] = f"set(Deadeye_VER {version}) # updated by scripts/bump.py\n"
-
-    with open(cmake, "w") as file:
-        file.writelines(lines)
+def displaymatch(match):
+    if match is None:
+        return None
+    return "<Match: %r, groups=%r>" % (match.group(), match.groups())
 
 
-def do_setup(version):
-    """ Bump setup.py version."""
-    setup = Path(__file__).parent / ".." / "admin-server" / "setup.py"
+class Version:
+    """Manage Deadeye version."""
 
-    with open(setup, "r") as file:
-        lines = file.readlines()
+    def __init__(self):
+        self.deadeye_dir = Path(__file__).parent / ".."
+        self.major_version = 0
+        self.minor_version = 0
+        self.patch_version = 0
 
-    assert lines[4].startswith("    version=")
-    lines[4] = f'    version="{version}",  # updated by scripts/bump.py\n'
+        version_parser = re.compile(r"set\(Deadeye_VER\s(\d+)\.(\d+)\.(\d+)")
+        cmake = self.deadeye_dir / "daemon" / "version.cmake"
+        with open(cmake, "r") as file:
+            for line in file.readlines():
+                match = version_parser.match(line)
+                # print(displaymatch(match))
+                if match:
+                    self.major_vers = int(match.group(1))
+                    self.minor_vers = int(match.group(2))
+                    self.patch_vers = int(match.group(3))
 
-    with open(setup, "w") as file:
-        file.writelines(lines)
+    def version(self):
+        return f"{self.major_vers}.{self.minor_vers}.{self.patch_vers}"
 
+    def bump_major(self):
+        self.major_vers = self.major_vers + 1
 
-def do_ansible(version):
-    """ Bump ansible vars version."""
-    vars = (
-        Path(__file__).parent
-        / ".."
-        / "ansible"
-        / "roles"
-        / "deadeye"
-        / "vars"
-        / "main.yaml"
-    )
+    def bump_minor(self):
+        self.minor_vers = self.minor_vers + 1
 
-    with open(vars, "r") as file:
-        lines = file.readlines()
+    def bump_patch(self):
+        self.patch_vers = self.patch_vers + 1
 
-    assert lines[1].startswith("deadeye_version:")
-    lines[1] = f"deadeye_version: {version} # updated by scripts/bump.py\n"
+    def update_daemon(self):
+        """ Bump cmake version file."""
+        cmake = self.deadeye_dir / "daemon" / "version.cmake"
 
-    with open(vars, "w") as file:
-        file.writelines(lines)
+        with open(cmake, "r") as file:
+            lines = file.readlines()
+
+        lines[0] = f"set(Deadeye_VER {self.version()}) # updated by scripts/bump.py\n"
+
+        with open(cmake, "w") as file:
+            file.writelines(lines)
+
+    def update_admin(self):
+        """ Bump setup.py version."""
+        setup = self.deadeye_dir / "admin-server" / "setup.py"
+
+        with open(setup, "r") as file:
+            lines = file.readlines()
+
+        assert lines[4].startswith("    version=")
+        lines[4] = f'    version="{self.version()}",  # updated by scripts/bump.py\n'
+
+        with open(setup, "w") as file:
+            file.writelines(lines)
+
+    def update_ansible(self):
+        """ Bump ansible vars version."""
+        ans = self.deadeye_dir / "ansible" / "roles" / "deadeye" / "vars" / "main.yaml"
+
+        with open(ans, "r") as file:
+            lines = file.readlines()
+
+        assert lines[1].startswith("deadeye_version:")
+        lines[1] = f"deadeye_version: {self.version()} # updated by scripts/bump.py\n"
+
+        with open(ans, "w") as file:
+            file.writelines(lines)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Bump Deadeye to specified version.")
-    parser.add_argument(
-        "version", metavar="VERSION", type=str, help="next version",
-    )
+    parser.add_argument("--major", action="store_true")
+    parser.add_argument("--minor", action="store_true")
+    parser.add_argument("--patch", action="store_true")
     args = parser.parse_args()
 
-    do_cmake(args.version)
-    do_setup(args.version)
-    do_ansible(args.version)
+    version = Version()
+    if args.major:
+        version.bump_major()
+    if args.minor:
+        version.bump_minor()
+    if args.patch:
+        version.bump_patch()
+
+    version.update_daemon()
+    version.update_admin()
+    version.update_ansible()
+
+    print(f"Updated version to {version.version()}")
