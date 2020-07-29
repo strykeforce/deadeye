501ecbf48e10cd9cd471c51611a7779d764846da
Jeff Hutchison <jeff@jeffhutchison.com>
2020-07-29 13:25:00 -0400

    Bump version to 20.3.0

--- admin-server/setup.py
+++ admin-server/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deadeye",
-    version="20.2.0",  # keep in sync with ansible/roles/deadeye/vars/main.yaml, daemon/version.cmake
+    version="20.3.0",  # updated by scripts/bump.py
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
--- daemon/version.cmake
+++ daemon/version.cmake
@@ -1,1 +1,1 @@
-set(Deadeye_VER 20.2.0) # keep in sync with admin-server/setup.py, ansible/roles/deadeye/vars/main.yaml
+set(Deadeye_VER 20.3.0) # updated by scripts/bump.py
