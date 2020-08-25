7fed6ecca3fed9d1446127611920b165161cac96
Jeff Hutchison <jeff@jeffhutchison.com>
2020-08-25 18:49:50 -0400

    Make admin server port configurable

--- admin-server/deadeye/__main__.py
+++ admin-server/deadeye/__main__.py
@@ -11,6 +11,8 @@ logging.getLogger("deadeye").setLevel(
     logging.INFO if os.environ["FLASK_ENV"] == "production" else logging.DEBUG
 )
 
+PORT = os.environ["DEADEYE_ADMIN_PORT"] if "DEADEYE_ADMIN_PORT" in os.environ else 5000
+
 APP = create_app()
 API = Api(APP)
-API.socketio.run(APP, host="0.0.0.0")
+API.socketio.run(APP, host="0.0.0.0", port=PORT)
