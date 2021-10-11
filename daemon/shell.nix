a0d131fbe2cd3ea9e9fdf8474c6fa0e7e0ca26f9
Jeff Hutchison <jeff@jeffhutchison.com>
2021-10-11 06:18:22 -0400

    Be specific about where pkgs come from

--- daemon/shell.nix
+++ daemon/shell.nix
@@ -1,22 +1,22 @@
 { sources ? import nix/sources.nix }:
 let
   pkgs = import sources.nixpkgs { };
-  wpilib = pkgs.callPackage ./nix/wpilib.nix { inherit sources; };
   libsafe = pkgs.callPackage ./nix/safe.nix { inherit sources; };
-  tinyfsm = pkgs.callPackage ./nix/tinyfsm.nix { inherit sources; };
   readerwriterqueue = pkgs.callPackage ./nix/readerwriterqueue.nix { inherit sources; };
+  tinyfsm = pkgs.callPackage ./nix/tinyfsm.nix { inherit sources; };
+  wpilib = pkgs.callPackage ./nix/wpilib.nix { inherit sources; };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    catch2
-    cmakeCurses
+  buildInputs = [
+    pkgs.catch2
+    pkgs.cmakeCurses
     libsafe
-    ninja
-    nlohmann_json
-    pkg-config
+    pkgs.ninja
+    pkgs.nlohmann_json
+    pkgs.pkg-config
     readerwriterqueue
-    spdlog
-    systemd.dev
+    pkgs.spdlog
+    pkgs.systemd.dev
     tinyfsm
     wpilib
   ];
