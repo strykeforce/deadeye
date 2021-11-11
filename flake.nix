665b165644dadcd3721e496a7c301893a11944d6
Jeff Hutchison <jeff@jeffhutchison.com>
2021-11-11 08:01:25 -0500

    Add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Deadeye develop environment";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [ jdk11 ];
+        shellHook = ''
+          '';
+      };
+    };
+}
