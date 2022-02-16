7490871700cd9b4a3b49731ef3fe2c195d053cc1
Jeff Hutchison <jeff@jeffhutchison.com>
2022-02-16 16:10:23 -0500

    Add optional JSON config item to PipelineConfig

--- daemon/src/config/pipeline_config.cpp
+++ daemon/src/config/pipeline_config.cpp
@@ -7,7 +7,12 @@ using json = nlohmann::json;
 
 PipelineConfig::PipelineConfig(int sn, hsv_t hue, hsv_t sat, hsv_t val,
                                FilterConfig filter, LogConfig log)
-    : sn(sn), hue(hue), sat(sat), val(val), filter(filter), log(std::move(log)) {}
+    : sn(sn),
+      hue(hue),
+      sat(sat),
+      val(val),
+      filter(filter),
+      log(std::move(log)) {}
 
 PipelineConfig::PipelineConfig(const std::shared_ptr<nt::Value>& value) {
   assert(value);
@@ -30,6 +35,7 @@ void deadeye::to_json(json& j, const PipelineConfig& p) {
            {PipelineConfig::kHsvHueKey, p.hue},
            {PipelineConfig::kHsvSatKey, p.sat},
            {PipelineConfig::kHsvValKey, p.val},
+           {PipelineConfig::kConfigKey, p.config},
            {PipelineConfig::kFilterKey, p.filter},
            {PipelineConfig::kLogKey, p.log}};
 }
@@ -43,4 +49,6 @@ void deadeye::from_json(const json& j, PipelineConfig& p) {
     j.at(PipelineConfig::kFilterKey).get_to(p.filter);
   if (j.contains(PipelineConfig::kLogKey))
     j.at(PipelineConfig::kLogKey).get_to(p.log);
+  if (j.contains(PipelineConfig::kConfigKey))
+    p.config = j.at(PipelineConfig::kConfigKey).get<json>();
 }
