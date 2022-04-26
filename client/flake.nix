aecee7d5224f8b971f1a5dbbe829aaf0206828e4
Jeff Hutchison <jeff@jeffhutchison.com>
2022-04-26 15:51:41 -0400

    Remove poetry2nix from flakes

--- client/flake.nix
+++ client/flake.nix
@@ -4,7 +4,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
