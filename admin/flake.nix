30cd20665877e098c3a683f83f9fe982079f88e5
Jeff Hutchison <jeff@jeffhutchison.com>
2022-04-26 13:58:53 -0400

    Add flake

---
+++ admin/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.python36Packages.poetry
+          ];
+        };
+      });
+}
