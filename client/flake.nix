d8f09418bbe07a2fc323c6fab0285075f6f79ef9
Jeff Hutchison <jeff@jeffhutchison.com>
2022-04-26 15:25:03 -0400

    Add flake for client

---
+++ client/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Deadeye Java client library";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.jdk11
+          ];
+        };
+      });
+}
