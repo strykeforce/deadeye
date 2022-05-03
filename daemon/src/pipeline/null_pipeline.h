ad16fcc0e1ed29df99b9947560b44d99a15861ee
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-03 08:36:00 -0400

    Allow specifying NullPipeline in daemon build

--- daemon/src/pipeline/null_pipeline.h
+++ daemon/src/pipeline/null_pipeline.h
@@ -11,9 +11,10 @@
 namespace deadeye {
 class NullPipeline : public Pipeline {
  public:
-  explicit NullPipeline(int inum)
-      : Pipeline{inum, "NullPipeline"},
+  NullPipeline(int inum, std::string name): Pipeline{inum, name},
         id_{DEADEYE_UNIT + std::to_string(inum_)} {}
+
+  explicit NullPipeline(int inum): NullPipeline(inum, "NullPipeline") {}
 
   void Configure(CaptureConfig const& config) override {}
   void Configure(PipelineConfig const& config) override {}
