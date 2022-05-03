11ea03fe4eb36bb5dbe133bdafa1b7f2e7163217
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-03 08:36:30 -0400

    Update flake to build with gstreamer plugins in path

--- daemon/flake.nix
+++ daemon/flake.nix
@@ -88,7 +88,6 @@
               pkgs.catch2
               pkgs.gst_all_1.gst-plugins-base
               pkgs.gst_all_1.gst-plugins-good
-              pkgs.gst_all_1.gstreamer.dev
               pkgs.nlohmann_json
               pkgs.pkg-config
               pkgs.spdlog.dev
@@ -101,39 +100,45 @@
             ];
           };
 
-          packages.default = pkgs.stdenv.mkDerivation {
-            pname = "deadeye-daemon";
-            version = "22.2.0";
-            src = ./.;
-            buildInputs = [
-              pkgs.cmake
-              pkgs.gcc-unwrapped
-              pkgs.catch2
-              pkgs.nlohmann_json
-              pkgs.pkg-config
-              pkgs.spdlog.dev
-              wpilib
-              (buildHeaderOnlyLib "readerwriterqueue" "1.0.5" readerwriterqueue)
-              (buildHeaderOnlyLib "safe" "1.0.0" safe)
-              (buildHeaderOnlyLib "tinyfsm" "0.3.3" tinyfsm)
-            ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
-              pkgs.systemd.dev
-            ];
+          packages.default =
+            let
+              gst = with pkgs.gst_all_1; [ gstreamer gst-plugins-base gst-plugins-good ];
+              gstPluginPath = "--prefix GST_PLUGIN_PATH : ${pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" gst}";
+            in
+            pkgs.stdenv.mkDerivation rec {
+              pname = "deadeye-daemon";
+              version = "22.2.0";
+              src = ./.;
 
-            propagatedBuildInputs = [
-              pkgs.gst_all_1.gstreamer.dev
-              pkgs.gst_all_1.gst-plugins-base
-              pkgs.gst_all_1.gst-plugins-good
-            ];
+              nativeBuildInputs = [
+                pkgs.cmake
+                pkgs.pkg-config
+                pkgs.makeWrapper
+              ];
 
-            cmakeFlags = [
-              "-DCMAKE_BUILD_TYPE=Release"
-              "-DDEADEYE_UNIT_ID=N"
-              "-DDEADEYE_CAMERA0_PIPELINE=deadeye::UprightRectPipeline"
-              "-DDEADEYE_CAMERA1_PIPELINE=deadeye::MinAreaRectPipeline"
-              "-DDEADEYE_CAMERA2_PIPELINE=deadeye::TargetListPipeline"
-            ];
-          };
+              buildInputs = [
+                pkgs.catch2
+                pkgs.nlohmann_json
+                pkgs.spdlog.dev
+                wpilib
+                (buildHeaderOnlyLib "readerwriterqueue" "1.0.5" readerwriterqueue)
+                (buildHeaderOnlyLib "safe" "1.0.0" safe)
+                (buildHeaderOnlyLib "tinyfsm" "0.3.3" tinyfsm)
+              ] ++ pkgs.lib.optional pkgs.stdenv.isLinux pkgs.systemd.dev;
+
+              cmakeFlags = [
+                "-DCMAKE_BUILD_TYPE=Release"
+                "-DDEADEYE_BUILD_TESTS=OFF"
+                "-DDEADEYE_BUILD_BENCHMARKS=OFF"
+                "-DDEADEYE_UNIT_ID=N"
+                "-DDEADEYE_NT_SERVER=127.0.0.1"
+                "-DDEADEYE_CAMERA0_PIPELINE=deadeye::NullPipeline"
+              ];
+
+              postFixup = ''
+                wrapProgram "$out/bin/deadeyed" ${gstPluginPath}
+              '';
+            };
         }
       ) // {
       nixosModules.default = { config, lib, pkgs, ... }:
@@ -144,6 +149,54 @@
         {
           options.deadeye.daemon = {
             enable = mkEnableOption "Enable the Deadeye pipeline daemon service";
+
+            unitId = mkOption {
+              description = "Deadeye Unit ID";
+              type = types.str;
+              default = "Z";
+              example = "A";
+            };
+
+            pipeline0 = mkOption {
+              description = "Deadeye Pipeline 0";
+              type = types.str;
+              default = "deadeye::NullPipeline";
+              example = "deadeye::UprightRectPipeline";
+            };
+
+            pipeline1 = mkOption {
+              description = "Deadeye Pipeline 0";
+              type = types.str;
+              default = "";
+              example = "deadeye::UprightRectPipeline";
+            };
+
+            pipeline2 = mkOption {
+              description = "Deadeye Pipeline 0";
+              type = types.str;
+              default = "";
+              example = "deadeye::UprightRectPipeline";
+            };
+
+            pipeline3 = mkOption {
+              description = "Deadeye Pipeline 0";
+              type = types.str;
+              default = "";
+              example = "deadeye::UprightRectPipeline";
+            };
+
+            pipeline4 = mkOption {
+              description = "Deadeye Pipeline 0";
+              type = types.str;
+              default = "";
+              example = "deadeye::UprightRectPipeline";
+            };
+
+            streamAddress = mkOption {
+              description = "Address of the camera stream.";
+              type = types.str;
+              default = "0.0.0.0";
+            };
 
             ntServerAddress = mkOption {
               description = "Address of the NetworkTables server";
@@ -156,6 +209,13 @@
               type = types.port;
               default = 1735;
             };
+
+            logLevel = mkOption {
+              description = "Logging level";
+              type = types.enum [ "trace" "debug" "info" "warn" "error" "critical" ];
+              default = "info";
+              example = "debug";
+            };
           };
 
           config = mkIf cfg.enable {
@@ -165,11 +225,25 @@
               environment = {
                 DEADEYE_NT_PORT = "${toString cfg.ntServerPort}";
                 DEADEYE_NT_SERVER = cfg.ntServerAddress;
-                SPDLOG_LEVEL = "info";
+                SPDLOG_LEVEL = cfg.logLevel;
               };
 
               serviceConfig =
-                let pkg = self.packages.${pkgs.system}.default;
+                let pkg = self.packages.${pkgs.system}.default.overrideAttrs (oldAttrs: rec {
+                  cmakeFlags = [
+                    "-DCMAKE_BUILD_TYPE=Release"
+                    "-DDEADEYE_BUILD_TESTS=OFF"
+                    "-DDEADEYE_BUILD_BENCHMARKS=OFF"
+                    "-DDEADEYE_UNIT_ID=${cfg.unitId}"
+                    "-DDEADEYE_NT_SERVER=${cfg.ntServerAddress}"
+                    "-DDEADEYE_CAMERA0_PIPELINE=${cfg.pipeline0}"
+                    "-DDEADEYE_CAMERA1_PIPELINE=${cfg.pipeline1}"
+                    "-DDEADEYE_CAMERA2_PIPELINE=${cfg.pipeline2}"
+                    "-DDEADEYE_CAMERA3_PIPELINE=${cfg.pipeline3}"
+                    "-DDEADEYE_CAMERA4_PIPELINE=${cfg.pipeline4}"
+                    "-DDEADEYE_STREAM_ADDRESS=${cfg.streamAddress}"
+                  ];
+                });
                 in
                 {
                   Restart = "on-failure";
@@ -179,22 +253,25 @@
           };
         };
       #
-      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          self.nixosModules.default
-          ({ config, pkgs, ... }: {
-            # Only allow this to boot as a container
-            boot.isContainer = true;
-            networking.hostName = "deadeye";
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem
+        {
+          system = "x86_64-linux";
+          modules = [
+            self.nixosModules.default
+            ({ config, pkgs, lib, ... }: {
+              # Only allow this to boot as a container
+              boot.isContainer = true;
+              networking.hostName = "deadeye";
 
-            # Allow nginx through the firewall
-            # networking.firewall.allowedTCPPorts = [ config.deadeye.admin.port ];
-
-            deadeye.daemon.enable = true;
-            deadeye.daemon.ntServerAddress = "192.168.1.30";
-          })
-        ];
-      };
+              deadeye.daemon.enable = true;
+              deadeye.daemon.unitId = "A";
+              deadeye.daemon.pipeline0 = "deadeye::MinAreaRectPipeline";
+              deadeye.daemon.pipeline1 = "deadeye::UprightRectPipeline";
+              deadeye.daemon.ntServerAddress = "192.168.1.30";
+              deadeye.daemon.logLevel = "trace";
+              deadeye.daemon.streamAddress = "0.0.0.0";
+            })
+          ];
+        };
     };
 }
