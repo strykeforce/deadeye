3d79267b9b787f67e563a48796419602c335ab34
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-03 21:40:28 -0400

    Add React strict mode

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />); // TODO add strict
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
