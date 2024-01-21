beef01550477c9cf0cfb18199eee8eff390a53a8
Jeff Hutchison <jeff@jeffhutchison.com>
2024-01-21 10:27:05 -0500

    Add flake to run Ansible

---
+++ ansible/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    let out = system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ansible
+            ansible-lint
+          ];
+        };
+
+      };
+    in with utils.lib; eachSystem defaultSystems out;
+}
