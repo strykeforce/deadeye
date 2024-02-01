bfee399d8457a24f9c360ab5820135dd9f367e07
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-01 15:49:43 -0500

    Update flake to use poetry2nix

--- admin/flake.nix
+++ admin/flake.nix
@@ -1,23 +1,37 @@
 {
+  description = "Deadeye admin server";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      # url = "github:nix-community/poetry2nix";
+      url = "github:jhh/poetry2nix/pynetworktables";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    let
-      out = system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              poetry
-            ];
-          };
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv mkPoetryApplication;
+      in
+      {
+        packages = {
+          deadeye-admin = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.deadeye-admin;
 
+          venv = mkPoetryEnv
+            {
+              projectDir = self;
+              groups = [ "main" "dev" ];
+            };
         };
-    in
-    with utils.lib; eachSystem defaultSystems out;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.deadeye-admin ];
+          packages = [ pkgs.poetry ];
+        };
+      });
 }
