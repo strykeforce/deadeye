5ade6f45f598bc7c72ac8cee90f196716c159d46
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-01 21:31:14 -0500

    Enable nix dockerTools image build

--- admin/flake.nix
+++ admin/flake.nix
@@ -12,26 +12,35 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv mkPoetryApplication;
-      in
-      {
-        packages = {
-          deadeye-admin = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.deadeye-admin;
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv mkPoetryApplication;
+        in
+        {
+          packages = {
+            deadeye-admin = mkPoetryApplication { projectDir = self; };
+            default = self.packages.${system}.deadeye-admin;
 
-          venv = mkPoetryEnv
-            {
-              projectDir = self;
-              groups = [ "main" "dev" ];
+            venv = mkPoetryEnv
+              {
+                projectDir = self;
+                groups = [ "main" "dev" ];
+              };
+
+            docker = pkgs.dockerTools.buildImage {
+              name = "deadeye-admin";
+              tag = "latest";
+              copyToRoot = self.packages.${system}.deadeye-admin.dependencyEnv
+              ;
+              config.Cmd = [ "/bin/deadeye-server" ];
             };
-        };
+          };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.deadeye-admin ];
-          packages = [ pkgs.poetry ];
-        };
-      });
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.deadeye-admin ];
+            packages = [ pkgs.poetry ];
+          };
+        });
 }
