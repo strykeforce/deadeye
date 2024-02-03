66977e36edbdba5cc538698bd77b25287a7e4aee
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-02 20:45:13 -0500

    Move nginx.conf out of flake

--- web/flake.nix
+++ web/flake.nix
@@ -38,71 +38,16 @@
             let
               nginxPort = "80";
               nginxWebRoot = self.packages.${system}.deadeye-web;
-              nginxConf = pkgs.writeText "nginx.conf" ''
-                user nobody nobody;
-                daemon off;
-                error_log /dev/stdout info;
-                pid /dev/null;
-                events {}
-                http {
-                  include ${pkgs.nginx}/conf/mime.types;
-                  access_log /dev/stdout;
-                  server {
-                    listen ${nginxPort};
-
-                    location / {
-                      root ${nginxWebRoot};
-                      index index.html;
-                      try_files $uri $uri/ /index.html;
-                    }
-
-                    location /socket.io/ {
-                      proxy_set_header Upgrade $http_upgrade;
-                      proxy_set_header Connection "Upgrade";
-                      proxy_http_version 1.1;
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://admin:5000;
-                    }
-
-                    location /upload {
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://admin:5000;
-                    }
-
-                    location /stream/0/ {
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://daemon:5805/stream.mjpg;
-                    }
-
-                    location /stream/1/ {
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://daemon:5806/stream.mjpg;
-                    }
-
-                    location /stream/2/ {
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://daemon:5807/stream.mjpg;
-                    }
-
-                    location /stream/3/ {
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://daemon:5808/stream.mjpg;
-                    }
-
-                    location /stream/4/ {
-                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                      proxy_set_header Host $host;
-                      proxy_pass http://daemon:5809/stream.mjpg;
-                    }
-                  }
-                }
-              '';
+              nginxConf = pkgs.writeText "nginx.conf" (builtins.replaceStrings [
+                "%NGINX_PORT%"
+                "%WEB_ROOT%"
+                "%MIME_TYPES%"
+              ] [
+                "${nginxPort}"
+                "${nginxWebRoot}"
+                "${pkgs.nginx}/conf/mime.types"
+              ]
+                (builtins.readFile ./nginx.conf));
             in
             pkgs.dockerTools.buildLayeredImage {
               name = "j3ff/deadeye-web";
@@ -120,9 +65,7 @@
 
               config = {
                 Cmd = [ "nginx" "-c" nginxConf ];
-                ExposedPorts = {
-                  "${nginxPort}/tcp" = { };
-                };
+                ExposedPorts = { "${nginxPort}/tcp" = { }; };
               };
             };
 
@@ -130,7 +73,7 @@
 
           #
           devShells.default = pkgs.mkShell {
-            # inputsFrom = [ self.packages.${system}.deadeye-admin ];
+            # inputsFrom = [ self.packages.${system}  .deadeye-admin ];
             packages = with pkgs; [ just nodejs_18 node2nix ];
           };
         });
